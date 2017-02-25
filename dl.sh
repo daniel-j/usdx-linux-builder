ff5de1596c542dac1386e54396584e70b914a213
daniel-j <daniel.j.hede@gmail.com>
2017-02-25 23:35:48 +0100

    Use USDX repo

--- dl.sh
+++ dl.sh
@@ -6,9 +6,9 @@ mkdir -p src
 cd src
 echo "Downloading USDX"
 mkdir -p "USDX"
-#curl -L "https://github.com/UltraStar-Deluxe/USDX/archive/$1.tar.gz" | tar -xz -C "USDX" --strip-components=1
-# temporary fix
-curl -L "https://github.com/daniel-j/USDX/archive/master.tar.gz" | tar -xz -C "USDX" --strip-components=1
+curl -L "https://github.com/UltraStar-Deluxe/USDX/archive/$1.tar.gz" | tar -xz -C "USDX" --strip-components=1
+# use fork
+#curl -L "https://github.com/daniel-j/USDX/archive/master.tar.gz" | tar -xz -C "USDX" --strip-components=1
 find "USDX" -type f -name "*.dll" -exec rm -f {} \;
 
 echo "Downloading SDL2"
